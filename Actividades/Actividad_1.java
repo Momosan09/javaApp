ccc27bb72e57c4e2d51b8ba40cc6d72f8db30faa
Momosan09 <gaelpaezg@gmail.com>
2022-12-21 13:13:38 -0300

    Anda a medias

--- Actividades/Actividad_1.java
+++ Actividades/Actividad_1.java
@@ -7,79 +7,72 @@ import javax.swing.JPanel;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
 
-
 import Recursos.Radio;
 import Recursos.Button;
 import Recursos.Label;
 import Recursos.TextField;
 
 public class Actividad_1 extends SuperActividades {
-    JPanel jp1 = new JPanel();//jp1 es para los inputs
-    JPanel jp2 = new JPanel();//jp2 es para los outputs
-
+    JPanel jp1 = new JPanel();// jp1 es para los inputs
+    JPanel jp2 = new JPanel();// jp2 es para los outputs
 
     public float medidaTela, medidaTotalTela, precioTela, precioTotalTela, precioPano, precioConfeccion;
     public int cantidadPanos;
     public String medidaTelaOut;
 
-    
-    //Button btn1 = new Button("Boton 1", 200, 0, 100, 20);
-    
-    /*INPUT */
+    // Button btn1 = new Button("Boton 1", 200, 0, 100, 20);
 
-    /*Medida Tela*/
+    /* INPUT */
+
+    /* Medida Tela */
     JLabel lbMedidaTela = new Label("Ingrese medida de la tela", "Medida de la tela en cm", 0, 0, 250, 25, jp1);
-    TextField txfMedidaTela = new TextField(250, 0, 150, 25, jp1);
-    
-    /*Precio un solo pano*/
-    JLabel lbPrecioPanos = new Label("Precio de un paño", "Precio por unidad de paño", 0,30,250,25,jp1);
-    TextField txfPrecioUnPano = new TextField(250, 30, 150, 25, jp1);
+    TextField txfMedidaTela = new TextField(250, 0, 150, 25, 5, jp1, "Medida de la tela = ");
 
-    /*Precio Tela */
-    JLabel lbPrecioTela = new Label("Precio de la tela", "Precio de la tela", 0, 60, 250,25,jp1);
-    TextField txfPrecioTela = new TextField(250, 60, 150, 25, jp1);
+    /* Precio un solo pano */
+    JLabel lbPrecioPanos = new Label("Precio de un paño", "Precio por unidad de paño", 0, 30, 250, 25, jp1);
+    TextField txfPrecioUnPano = new TextField(250, 30, 150, 25, 5, jp1, "Precio de un paño = ");
 
-    /*Modelo elegido */
+    /* Precio Tela */
+    JLabel lbPrecioTela = new Label("Precio de la tela", "Precio de la tela", 0, 60, 250, 25, jp1);
+    TextField txfPrecioTela = new TextField(250, 60, 150, 25, 5, jp1, "Precio de la tela = ");
+
+    /* Modelo elegido */
     ButtonGroup bg = new ButtonGroup();
 
-    JRadioButton r1 = new Radio("Modelo 1",10,100,100,30,jp1);
-    JRadioButton r2 = new Radio("Modelo 2",10,130,100,30,jp1);
-    JRadioButton r3 = new Radio("Modelo 3",10,160,100,30,jp1);
+    JRadioButton r1 = new Radio("Modelo 1", 10, 100, 100, 30, jp1);
+    JRadioButton r2 = new Radio("Modelo 2", 10, 130, 100, 30, jp1);
+    JRadioButton r3 = new Radio("Modelo 3", 10, 160, 100, 30, jp1);
 
-    JRadioButton r4 = new Radio("Modelo 4",110,100,100,30,jp1);
-    JRadioButton r5 = new Radio("Modelo 5",110,130,100,30,jp1);
-    JRadioButton r6 = new Radio("Modelo 6",110,160,100,30,jp1);
+    JRadioButton r4 = new Radio("Modelo 4", 110, 100, 100, 30, jp1);
+    JRadioButton r5 = new Radio("Modelo 5", 110, 130, 100, 30, jp1);
+    JRadioButton r6 = new Radio("Modelo 6", 110, 160, 100, 30, jp1);
 
-    JRadioButton r7 = new Radio("Modelo 7",220,100,100,30,jp1);
-    JRadioButton r8 = new Radio("Modelo 8",220,130,100,30,jp1);
-    JRadioButton r9 = new Radio("Modelo 9",220,160,100,30,jp1);
+    JRadioButton r7 = new Radio("Modelo 7", 220, 100, 100, 30, jp1);
+    JRadioButton r8 = new Radio("Modelo 8", 220, 130, 100, 30, jp1);
+    JRadioButton r9 = new Radio("Modelo 9", 220, 160, 100, 30, jp1);
 
+    /* OUTPUT */
 
-    /*OUTPUT */
-
-    JLabel medidaOut = new Label("medida = " + medidaTelaOut, "medida de la tela en cm", 0,200,250,25, jp1);
-
-    
     public Actividad_1() {
         this.setTitle("Presupuesto 1");
         this.setVisible(true);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         ui();
         setValues();
-
         ponerPanel(jp1);
-    
-        
+
     }
 
     private void ui() {
         ponerEtiqueta();
         ponerRadio();
+        setValues();
+
     }
-    
-    private void ponerEtiqueta(){
+
+    private void ponerEtiqueta() {
         lbMedidaTela.setLabelFor(txfMedidaTela);
         lbPrecioPanos.setLabelFor(txfPrecioUnPano);
         lbPrecioTela.setLabelFor(txfPrecioTela);
@@ -97,7 +90,9 @@ public class Actividad_1 extends SuperActividades {
         bg.add(r8);
         bg.add(r9);
     }
-    private void setValues(){
+
+    private void setValues() {
+        // ver como hacer para mostrar los inputs
         medidaTela = txfMedidaTela.valor;
         precioPano = txfPrecioUnPano.valor;
         precioTela = txfPrecioTela.valor;
--- Recursos/TextField.java
+++ Recursos/TextField.java
@@ -1,51 +1,67 @@
 package Recursos;
+
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 import java.io.*;
 import java.awt.event.KeyEvent;//eventos de teclado
 import java.awt.event.KeyListener;//eventos de teclado
 
+public class TextField extends JTextField implements KeyListener {
 
-public class TextField extends JTextField implements KeyListener{
-  
   public float valor;
 
-  public TextField(int posX, int posY, int width, int height, JPanel jp){
-    setBounds(posX,posY,width,height);
-    jp.add(this);//se auto agrega al panel pasado como parametro
+  JLabel display = new JLabel();
 
+  public String textoDefault;
+
+  public TextField(int posX, int posY, int width, int height, int col, JPanel jp, String textoLabel) {
+
+    textoDefault = textoLabel;
+
+    setBounds(posX, posY, width, height);
+    setColumns(col);
+    dispayLabel(posX - posX, posY + 200, width, height, jp, textoLabel);
+    jp.add(this);// se auto agrega al panel pasado como parametro
     eventosTeclado();
   }
-  
-  private void eventosTeclado(){
-    
 
-    
-    this.addKeyListener(this);
-    
+  // Esto es para mostrar el valor en labels
+  // No me gusta mucho como quedo pero ya no se me ocurre nada mejor
+  private void dispayLabel(int posX, int posY, int width, int height, JPanel jp, String texto) {
+    display.setBounds(posX, posY, width, height);
+    display.setText(texto);
+    jp.add(display);
   }
 
+  private void eventosTeclado() {
+
+    addKeyListener(this);
+
+  }
 
   @Override
   public void keyTyped(KeyEvent e) {
     // TODO Auto-generated method stub
-    //System.out.println(getText());
-    
+    // System.out.println(getText());
+
   }
 
   @Override
   public void keyPressed(KeyEvent e) {
     // TODO Auto-generated method stub
-    //System.out.println(getText());
-    
+    // System.out.println(getText());
+
   }
 
   @Override
   public void keyReleased(KeyEvent e) {
     // TODO Auto-generated method stub
-    valor = Float.parseFloat(getText());
-    System.out.println(valor);
-  
-    
+    // valor = Float.parseFloat(getText());
+    // System.out.println(valor);
+
+    display.setText(textoDefault + getText());
+
   }
 }
