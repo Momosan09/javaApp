917928af95fd1862d87217e18f5eb559197073f3
Momosan09 <gaelpaezg@gmail.com>
2023-02-01 20:03:52 -0300

    Se puede insertar a la bd

--- Connect.java
+++ Connect.java
@@ -2,8 +2,11 @@
 /* si tiene "//" es porque es el codigo basico para que se ejecute, para no confundirme cuando crezca el codigo */
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.RowId;
+import java.sql.SQLException;
 
 public class Connect {
 
@@ -18,22 +21,61 @@ public class Connect {
             statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
             statement.executeUpdate("drop table if exists person");
-            statement.executeUpdate("create table person (id integer, name string)");
-            statement.executeUpdate("insert into person values(1, 'leo')");
-            statement.executeUpdate("insert into person values(2, 'yui')");
+            statement.executeUpdate("create table person (id integer PRIMARY KEY, name string)");
+            statement.executeUpdate("insert into person values(1,'leo')");
+            statement.executeUpdate("insert into person values(2,'yui')");
+            agregarPresupuesto("is something about you");
+            agregarPresupuesto("is hard to explain");
+            agregarPresupuesto("is around you boy");
+            agregarPresupuesto("but you still the same");
+
             ResultSet rs = statement.executeQuery("select * from person");
             while(rs.next())
             {
               // read the result set
-              System.out.println("name = " + rs.getString("name"));
               System.out.println("id = " + rs.getInt("id"));
+              System.out.println("name = " + rs.getString("name"));
             }
 
         } catch (Exception e) {//
-            System.out.println(e.getMessage());//
+            System.err.println(e.getMessage());//
         }
 
         return conecta;//
+    }
+
+    public void agregarPresupuesto(String name){
+        try (PreparedStatement Pstatement = conecta.prepareStatement("insert into person values(?,?)")) {
+
+            Pstatement.setInt(1, getIndex() + 1);
+            Pstatement.setString(2, name);
+            Pstatement.executeUpdate();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());//
+
+        }
+    }
+    private int getIndex(){
+        try{
+            int index = 0;
+            Statement statement = conecta.createStatement();
+
+            ResultSet rs = statement.executeQuery("select COUNT(id) from person");
+
+            while(rs.next()){
+                index = Integer.parseInt(rs.getString(1));
+                System.out.println("Indice = " + index);
+            }
+            return index;
+
+            
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+            return 0;
+        }
+
     }
 
 }
